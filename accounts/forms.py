858e6bdfbf7c1a11ba06063c3defdf361ac62fde
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-08-11 12:31:15 +0430

    register-user

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
+    email = forms.EmailField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
